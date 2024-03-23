963630038fa77030098b4e210698c4c8960938e6
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-03-23 17:36:16 +0100

    changes

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import useState from "react"
 
 const Movie = () => {
-  return (
-    <div>
-        
+
+    const [movieList,setMovieList]=useState([])
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MmNjMjJkMzk5Mzk0MjNmZTVmOGZjYWFjODdmYjM0OSIsInN1YiI6IjY1ZmVmNTM2NDU5YWQ2MDE2NGY3ZTQ4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IzdqRSP5FE2nWLxSyeJAAAGGpelQ20in10J_Nv8WhuA'
+        }
+      };
       
-    </div>
+      fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(response => setMovieList(response.results))
+        .catch(err => console.error(err));
+
+
+
+
+
+
+
+  return (
+    <>
+        <h1>Movie</h1>
+        {/* {movieList.map((movie)=>( */}
+{/* <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt=""/> */}
+
+        {/* ) */}
+
+        {/* )} */}
+
+
+      
+    </>
   )
 }
 
